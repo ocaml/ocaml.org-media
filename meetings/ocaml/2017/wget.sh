a202cb1f316bfffc87db9f4f4447a0d462fc3c09
Gabriel Scherer <gabriel.scherer@gmail.com>
2017-12-13 22:25:35 +0100

    OUD 2017 documents

---
+++ meetings/ocaml/2017/wget.sh
@@ -0,0 +1,36 @@
+# Run this script to refresh the local copies of the online documents,
+# in case the authors have made some changes
+
+wget http://www.tom-ridge.com/resources/ocaml_2017_slides.pdf \
+  -O slides__2017__tom-ridge__a-b-tree-library-for-ocaml.pdf
+
+wget http://www-scf.usc.edu/~zhanpenl/prog_syn.pdf \
+  -O extended-abstract__2017__zhanpeng-liang_kanae-tsushima__component-based-program-synthesis-in-ocaml.pdf
+
+wget https://www.cl.cam.ac.uk/~jdy22/papers/extending-ocamls-open.pdf \
+  -O extended-abstract__2017__runhang-li_jeremy-yallop__extending-ocaml-s-open.pdf
+
+wget http://wr.mondet.org/paper/smondet-genspio-ocaml17.pdf \
+  -O extended-abstract__2017__sebastien-mondet__genspio-generating-shell-phrases-in-ocaml.pdf
+
+wget https://www.cs.kent.ac.uk/people/staff/rnsr/docs/rotor_ocaml-17_abstract.pdf \
+  -O extended-abstract__2017__reuben-rowe_simon-thompson__rotor-first-steps-towards-a-refactoring-tool-for-ocaml.pdf
+
+wget https://www.cs.kent.ac.uk/people/staff/rnsr/docs/ocaml_2017_slides.pdf \
+  -O slides__2017__reuben-rowe_simon-thompson__rotor-first-steps-towards-a-refactoring-tool-for-ocaml.pdf
+
+wget https://g2p.github.io/research/wodan.pdf \
+  -O extended-abstract__2017__gabriel-de-perthuis__wodan-a-pure-ocaml-flash-aware-filesystem-library.pdf
+
+wget http://www.cs.le.ac.uk/people/jw642/ocamlworkshop.pdf \
+  -O extended-abstract__2017__john_whitington__ocamli-interpreted-ocaml.pdf
+
+wget https://gbury.eu/public/papers/icfp2017_msat.pdf \
+  -O extended-abstract__2017__guillaume-bury__msat-an-ocaml-sat-solver.pdf
+
+wget https://www.irif.fr/~gradanne/papers/tyre/abstract.pdf \
+  -O extended-abstract__2017__gabriel-radanne__tyre-typed-regular-expressions.pdf
+
+
+
+
